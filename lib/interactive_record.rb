ed8f33dc4f7fede9789e3a2cf23693e4b71dce6d
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-30 18:05:14 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -60,8 +60,6 @@ class InteractiveRecord
   end
 
   def self.find_by(arg)
-    binding.pry
-
     if arg.values[0].is_a? String
       self.find_by_name(arg.values[0])
     elsif arg.values[0].is_a? Integer
