dff2f4be615e773879dd991557c031c40aacc7a7
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-30 17:56:09 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -50,7 +50,7 @@ def save
 end
 
 def self.find_by_name(name)
-  sql = "SELECT * FROM #{self.table_name} WHERE name = 'Jan'"
+  sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
 binding.pry
   DB[:conn].execute(sql)
 end
